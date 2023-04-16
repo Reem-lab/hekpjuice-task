8113e57b132755c4b165476fc8cbe49419fd98b2
reem <janinareem@gmail.com>
2023-04-16 18:10:42 +0200

    Add additional features

--- script.js
+++ script.js
@@ -4,22 +4,34 @@ const exitIcon = document.querySelector('.exit');
 const navLinks = document.querySelectorAll('.nav-links li');
 const body = document.querySelector('body');
 
-function mobileMenu() {
+const closeBtn = document.querySelector('.closebtn')
+const topNavBar = document.querySelector('.top-navbar');
+const rightSideImg = document.querySelector('.rightSide-img')
+const closeImg = document.querySelector('.closeBtn-icon')
+
+closeBtn.addEventListener('click', () => {
+  topNavBar.classList.add('display-hidden');
+} )
+
+closeImg.addEventListener('click', () => {
+  rightSideImg.classList.add('rightSideImg-hidden');
+  // rightSideImg.classList.add('.display-hidden');
+})
+
+
+
+const removeMenu = () => {
+  mainNav.classList.remove('main-nav-active');
+  body.classList.remove('overflow-hidden');
+}
 
   menuIcon.addEventListener('click', () => {
     mainNav.classList.add('main-nav-active');
     body.classList.add('overflow-hidden');
   });
-  exitIcon.addEventListener('click', () => {
-    mainNav.classList.remove('main-nav-active');
-    body.classList.remove('overflow-hidden');
-  });
-  navLinks.forEach((link) => {
-    link.addEventListener('click', () => {
-      mainNav.classList.remove('main-nav-active');
-      body.classList.remove('overflow-hidden');
-    });
-  });
-}
 
-mobileMenu();
+  exitIcon.addEventListener('click', removeMenu);
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', removeMenu )
+  });
